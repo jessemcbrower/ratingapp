20db99782de3c7534b2fe8e240c5fb3605a79458
Jesse Brower <browerpowr@gmail.com>
2021-01-29 17:20:54 -0500

    refactored conditionals again and removed the global variable for customerid to make it optionally static instead

--- application.py
+++ application.py
@@ -6,51 +6,49 @@ import constant
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'dev'
 
-try:
-	customerid
-except NameError:
-	customerid = 1
+@application.template_filter()
+def currencyFormat(value):
+    value = float(value)
+    return "${:,.2f}".format(value)
 
 @application.route('/', methods=['POST', 'GET'])
-def getinfo():
+def get_info():
 
-	global customerid
 	form = RatingRequest()
 
 	if form.validate_on_submit():
 
-		data = getData()
-		print(data)
+		data = get_data()
 
-		dwelling = round(dwellingRate(data), 3)
-		age = homeAge(data)
-		units = unitsRate(data)
-		roof = roofRate(data)
-		discount = round(discountAmount(data), 2)
-		rate = premiumTotal(data)
+		customerid = data['id']
+		dwelling = round(get_dwelling_rate(data), 3)
+		age = get_home_age_rate(data)
+		units = get_units_rate(data)
+		roof = get_roof_rate(data)
+		discount = round(calculate_discount(data), 2)
+		rate = calculate_premium(data)
 
 		return render_template('premium.html',
-								form=form,
-								data=data, 
-								rate=rate,
-								dwelling=dwelling,
-								age=age,
-								units=units,
-								customerid=customerid,
-								roof=roof,
-								discount=discount)
+			form=form,
+			data=data, 
+			rate=rate,
+			dwelling=dwelling,
+			age=age,
+			units=units,
+			customerid=customerid,
+			roof=roof,
+			discount=discount)
 
 	return render_template('index.html',
-								form=form,
-								customerid=customerid)
+		form=form)
 
-def getData():
+def get_data():
 
 	data = {
 
-		'id': customerid,
+		'id': request.form.get('CustomerID'),
 		'coverage': request.form.get('DwellingCoverage'),
-		'age': int(request.form.get('HomeAge')),
+		'age': request.form.get('HomeAge'),
 		'roof': request.form.get('RoofType'),
 		'units': request.form.get('NumberOfUnits'),
 		'discount': request.form.get('PartnerDiscount')
@@ -59,43 +57,79 @@ def getData():
 
 	return data
 
-def roofRate(data):
+def get_roof_rate(data):
+
 	r = data['roof']
-	roof = 1.00 if r == 'asphault' else 1.70 if r == 'tin' else 2.00 if r == 'wood'	else None
+
+	if r == 'Asphault Shingles':
+		roof = 1.00
+	elif r == 'Tin':
+		roof = 1.70
+	elif r == 'Wood':
+		roof = 2.00
+
 	return roof
 
-def homeAge(data):
-	n = data['age']
-	age = 1.00 if 0 <= n <= 10 else 1.50 if 11 <= n <= 35 else 1.80 if 36 <= n <= 100 else 1.95 if n > 100 else None
+def get_home_age_rate(data):
+
+	n = int(data['age'])
+
+	if 0 <= n <= 10:
+		age = 1.00
+	elif 11 <= n <= 35:
+		age = 1.50
+	elif 36 <= n <= 100:
+		age = 1.80
+	elif n > 100:
+		age = 1.95
+
 	return age
 
-def unitsRate(data):
+def get_units_rate(data):
+
 	n = int(data['units'])
-	units = 1.00 if n == 1 else 0.80 if n >= 2 and n <= 4 else None
+
+	if n == 1:
+		units = 1.00
+	elif n >= 2 and n <= 4:
+		units = 0.80
+
 	return units
 
-def dwellingRate(data):
+def get_dwelling_rate(data):
+
 	n = data['coverage']
-	dwelling = np.interp( n, constant.DWELLING_COVERAGE, constant.RATING_FACTOR)
+
+	dwelling = np.interp(n, constant.DWELLING_COVERAGE, constant.RATING_FACTOR)
+
 	return dwelling
 
-def premiumSubtotal(data):
-	subtotal = 350 * dwellingRate(data) * homeAge(data) * roofRate(data) * unitsRate(data)
+def calculate_subtotal(data):
+
+	subtotal = 350 * get_dwelling_rate(data) * get_home_age_rate(data) * get_roof_rate(data) * get_units_rate(data)
+
 	return subtotal
 
-def discountAmount(data):
+def calculate_discount(data):
+
 	d = data['discount']
-	discount = premiumSubtotal(data) * 0.05 if d == 'Y' else 0
+
+	discount = calculate_subtotal(data) * 0.05 if d == 'Y' else 0
+
 	return discount
 
-def premiumTotal(data):
-	premium = round(premiumSubtotal(data) - discountAmount(data), 2)
+def calculate_premium(data):
+
+	premium = round(calculate_subtotal(data) - calculate_discount(data), 2)
+
 	return premium
 
 @application.route('/reset', methods=['GET'])
-def resetForm():
+def reset_form():
+
 	global customerid
 	form = RatingRequest()
+
 	if request.method == 'GET':
-		customerid += 1
+		# customerid += 1
 		return redirect('/')
--- forms.py
+++ forms.py
@@ -4,9 +4,10 @@ from wtforms import IntegerField, SelectField, SubmitField
 from wtforms.validators import InputRequired, NumberRange, ValidationError
 
 class RatingRequest(FlaskForm):
-	DwellingCoverage = IntegerField('DwellingCoverage', validators=[InputRequired(), NumberRange(min=0, message='Must enter a value greater than zero')])
-	HomeAge =  IntegerField('HomeAge', validators=[InputRequired(), NumberRange(min=0, message='Must enter a value greater than zero')])
-	RoofType =  SelectField('RoofType', choices=[('asphault', 'Asphault Shingles'), ('tin', 'Tin'), ('wood', 'Wood')], validators=[InputRequired()])
+	CustomerID = IntegerField('CustomerID', validators=[InputRequired()], default=1)
+	DwellingCoverage = IntegerField('DwellingCoverage', validators=[InputRequired(), NumberRange(min=0, max=350000, message='Coverage must be between $0 and $350,000')])
+	HomeAge =  IntegerField('HomeAge', validators=[InputRequired(), NumberRange(min=0, message='Home age cannot be less than 0')])
+	RoofType =  SelectField('RoofType', choices=[('Asphault Shingles', 'Asphault Shingles'), ('Tin', 'Tin'), ('Wood', 'Wood')], validators=[InputRequired()])
 	NumberOfUnits =  SelectField('NumberOfUnits', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')], validators=[InputRequired()])
 	PartnerDiscount =  SelectField('PartnerDiscount', choices=[('Y', 'Y'), ('N', 'N')], validators=[InputRequired()])
 	Calculate = SubmitField('Calculate')
